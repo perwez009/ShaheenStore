5be6d895a360055613827e99f0681114e6ecf028
Shaheen <perwez58@gmail.com>
2025-12-21 22:32:28 +0530

    Fix CSS by removing basePath for GitHub Pages docs

--- docs/_next/static/Q46CSPGPOER7hV2oZbvMH/_buildManifest.js
+++ docs/_next/static/Q46CSPGPOER7hV2oZbvMH/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ShaheenStore//_next/:path+",
-        "destination": "/ShaheenStore/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
